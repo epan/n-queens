911a006645497bd993513e708f31954b48a809e0
MKS-Student <mks.student@hackreactor.com>
2017-03-30 11:20:37 -0700

    Remove comments from countNRooksSolutions

--- src/solvers.js
+++ src/solvers.js
@@ -39,36 +39,20 @@ window.findNRooksSolution = function(n) {
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 
 window.countNRooksSolutions = function(n) {
-  // Input: n integer
-  // Output: solutionCount integer
-
-  // Edge cases:
-  // if (n === 0) { return 0; }
   if (n === 1) { return 1; }
 
-  // Create new board of n by n
   var game = new Board({n: n});
-
-  // Initialize solution count to 0
-  // Initialize pieces count to 0
   var solutionCount = 0;
   var piecesCount = 0;
 
-  // Recursive solver function, arg: a row
-    // If the row = n && pieces = n
-      // Increment solution count
-      // Set pieces to 0
-      // return
   var solver = function (rowIndex) {
     if (rowIndex === n ) {
       solutionCount++;
-      //piecesCount = 0;
       return;
     }
     for (var colIndex = 0; colIndex < n; colIndex++) {
       game.togglePiece(rowIndex, colIndex);
       if (!game.hasAnyRooksConflicts()) {
-        //piecesCount++;
         solver(rowIndex + 1);
       }
       game.togglePiece(rowIndex, colIndex);
@@ -76,18 +60,6 @@ window.countNRooksSolutions = function(n) {
   };
 
   solver(0);
-
-    // Loop through row i, (row 0 row n)
-    // for (var colIndex = 0; colIndex < n; colIndex++)
-      // Toggle(row, i) ON
-      // If no conflicts on currently ON piece,
-        // increment pieces by 1
-        // Recurse solver function (row + 1)
-      // Toggle(row, i) OFF
-    // Invoke recursive solver with (0)
-
-
-
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
